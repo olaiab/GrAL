dc2d4f7fff895a3125dae59a405cefda459b7b72
olaiab <olaiaehu@gmail.com>
2026-05-02 20:40:12 +0200

    EXTI ez doa, baino zenbakien pantaila baiiiii

---
+++ tutorialak/UART2/STM32CubeIDE/Debug/Drivers/CMSIS/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+/home/olaia/Escritorio/GrAL/GitHub/tutorialak/UART2/Core/Src/system_stm32wlxx.c 
+
+OBJS += \
+./Drivers/CMSIS/system_stm32wlxx.o 
+
+C_DEPS += \
+./Drivers/CMSIS/system_stm32wlxx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/CMSIS/system_stm32wlxx.o: /home/olaia/Escritorio/GrAL/GitHub/tutorialak/UART2/Core/Src/system_stm32wlxx.c Drivers/CMSIS/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_NUCLEO_64 -DCORE_CM4 -DUSE_HAL_DRIVER -DSTM32WL55xx -c -I../../Core/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc -I../../Drivers/STM32WLxx_HAL_Driver/Inc/Legacy -I../../Drivers/BSP/STM32WLxx_Nucleo -I../../Drivers/CMSIS/Device/ST/STM32WLxx/Include -I../../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Drivers-2f-CMSIS
+
+clean-Drivers-2f-CMSIS:
+	-$(RM) ./Drivers/CMSIS/system_stm32wlxx.cyclo ./Drivers/CMSIS/system_stm32wlxx.d ./Drivers/CMSIS/system_stm32wlxx.o ./Drivers/CMSIS/system_stm32wlxx.su
+
+.PHONY: clean-Drivers-2f-CMSIS
+
